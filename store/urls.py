7476a48dd7516caf697f172f042eaa682a171596
egorpon <ponomarenkoegor36@gmail.com>
2025-10-21 17:07:27 +0300

    add list category view

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.store, name='store'),
-    path('product/<slug:slug>/', view=views.product_info, name='product_info')
+    path("", view=views.store, name="store"),
+    path("product/<slug:product_slug>/", view=views.product_info, name="product_info"),
+    path("search/<slug:category_slug>/", view=views.list_category, name="list_category"),
 ]
