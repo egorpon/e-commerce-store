654f913f925d5e05723da5e3d208f5b2ef3381e3
egorpon <ponomarenkoegor36@gmail.com>
2025-10-21 15:22:03 +0300

    add singular product page

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.store, name='store')
+    path('', view=views.store, name='store'),
+    path('product/<slug:slug>/', view=views.product_info, name='product_info')
 ]
