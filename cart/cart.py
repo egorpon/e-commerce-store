3e95cd4b152550ac229d91d582ca78258fdf2162
egorpon <ponomarenkoegor36@gmail.com>
2025-10-28 17:45:03 +0200

    create a session

---
+++ cart/cart.py
@@ -0,0 +1,11 @@
+class Cart():
+
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('cart')
+
+        if 'cart' not in request.session:
+
+            cart = self.session['cart'] = {}
+
+        self.cart = cart
