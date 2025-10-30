7f3183f290b346dc0ec13a81b8b8787c940c6a39
egorpon <ponomarenkoegor36@gmail.com>
2025-10-30 16:47:41 +0200

    finish cart summary template and add functionality

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+
+from store.models import Product
+
 class Cart():
 
     def __init__(self, request):
@@ -7,5 +11,53 @@ class Cart():
         if 'cart' not in request.session:
 
             cart = self.session['cart'] = {}
-
         self.cart = cart
+
+    def add(self, product, product_quantity):
+
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] = product_quantity
+        else:
+            self.cart[product_id] = {'price': str(product.price),
+                                     'quantity': product_quantity}
+            
+        self.session.modified = True
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+    
+
+    def __iter__(self):
+        all_products_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=all_products_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] =  product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total'] = item['price'] * item['quantity']
+            yield item
+
+    def get_total(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+d = {"cart" : {
+        "1":{
+            "price": Decimal(2.99),
+            "quantity": 3,
+            "product": Product(...),
+            "total": 8.97
+        },
+        "3":{
+            "price":Decimal(19.99),
+            "quantity": 1,
+            "product": Product(...),
+            "total": 19.99
+        }
+    } }
