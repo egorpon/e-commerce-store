ee5eca8978360e4af5aeb9a3059c12ae8c8fcaef
egorpon <ponomarenkoegor36@gmail.com>
2025-11-03 17:23:10 +0200

    add update and delete functionality

--- cart/cart.py
+++ cart/cart.py
@@ -2,32 +2,46 @@ from decimal import Decimal
 
 from store.models import Product
 
-class Cart():
 
+class Cart:
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get('cart')
+        cart = self.session.get("cart")
 
-        if 'cart' not in request.session:
-
-            cart = self.session['cart'] = {}
+        if "cart" not in request.session:
+            cart = self.session["cart"] = {}
         self.cart = cart
 
     def add(self, product, product_quantity):
-
         product_id = str(product.id)
 
         if product_id in self.cart:
-            self.cart[product_id]['quantity'] = product_quantity
+            self.cart[product_id]["quantity"] = product_quantity
         else:
-            self.cart[product_id] = {'price': str(product.price),
-                                     'quantity': product_quantity}
-            
+            self.cart[product_id] = {
+                "price": str(product.price),
+                "quantity": product_quantity,
+            }
+
+        self.session.modified = True
+
+    def delete(self, product_id):
+        product_id = str(product_id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+    def update(self, product_id, product_quantity):
+        product_id = str(product_id)
+        product_quantity = int(product_quantity)
+        if product_id in self.cart:
+            self.cart[product_id]["quantity"] = product_quantity
         self.session.modified = True
 
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
-    
+        return sum(item["quantity"] for item in self.cart.values())
 
     def __iter__(self):
         all_products_ids = self.cart.keys()
@@ -37,27 +51,32 @@ class Cart():
         cart = self.cart.copy()
 
         for product in products:
-            cart[str(product.id)]['product'] =  product
+            cart[str(product.id)]["product"] = product
 
         for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total'] = item['price'] * item['quantity']
+            item["price"] = Decimal(item["price"])
+            item["total"] = item["price"] * item["quantity"]
             yield item
 
     def get_total(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+        return sum(
+            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
+        )
 
-d = {"cart" : {
-        "1":{
+
+d = {
+    "cart": {
+        "1": {
             "price": Decimal(2.99),
             "quantity": 3,
             "product": Product(...),
-            "total": 8.97
+            "total": 8.97,
         },
-        "3":{
-            "price":Decimal(19.99),
+        "3": {
+            "price": Decimal(19.99),
             "quantity": 1,
             "product": Product(...),
-            "total": 19.99
-        }
-    } }
+            "total": 19.99,
+        },
+    }
+}
--- cart/views.py
+++ cart/views.py
@@ -32,8 +32,33 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+
+        cart.delete(product_id)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({"quantity": cart_quantity, "total": cart_total})
+        return response
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        product_quantity = int(request.POST.get("product_quantity"))
+        cart.update(product_id, product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'quantity': cart_quantity, 'total': cart_total})
+
+        return response
