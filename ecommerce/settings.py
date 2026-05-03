0f592ab8ffdd68fc624046fcdfcd4f85a7abf51f
egorpon <ponomarenkoegor36@gmail.com>
2026-05-03 15:18:52 +0300

    config: setugp allowed hosts for elastic ip and domain

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "3.237.18.67"]
+ALLOWED_HOSTS = ["127.0.0.1", "18.234.15.208","ecommercepro.site", "www.ecommercepro.site",]
 
 
 # Application definition
